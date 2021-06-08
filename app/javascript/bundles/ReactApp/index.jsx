749a79bd69232d1d2646ee6efbcc491275942f04
Alexander Veligurov <av@solteca.cl>
2021-06-08 14:26:39 +0300

    add basic react app and react-router-dom with routes

--- app/javascript/bundles/ReactApp/index.jsx
+++ app/javascript/bundles/ReactApp/index.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, StaticRouter } from 'react-router-dom';
+import Routes from './Routes';
 
-function ReactApp() {
-  return (
-    <div>
-      React App
-    </div>
-  )
+const Router = window !== undefined ? BrowserRouter : StaticRouter;
+
+function ReactApp(_, railsContext) {
+  return () => (
+    <Router location={railsContext?.location}>
+      <main>
+        <Routes />
+      </main>
+    </Router>
+  );
 }
 
-export default ReactApp
+export default ReactApp;
