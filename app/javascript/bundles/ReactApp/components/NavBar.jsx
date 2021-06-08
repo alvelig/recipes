db2e9210778959603d6ed12fccb38ebfa1ca6ba3
Alexander Veligurov <av@solteca.cl>
2021-06-08 18:49:53 +0300

    remove search

--- app/javascript/bundles/ReactApp/components/NavBar.jsx
+++ app/javascript/bundles/ReactApp/components/NavBar.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
@@ -12,10 +9,6 @@ function NavigationMenu() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto" />
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
       </Navbar.Collapse>
     </Navbar>
   );
